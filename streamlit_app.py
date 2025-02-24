d1fa4b659bf6915f450a2b2fb58b58ae1bfe89a4
adhikaribibek231 <159928438+adhikaribibek231@users.noreply.github.com>
2025-02-24 18:15:23 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
-
+import pandas as pd
 st.title('Penguins')
 
 st.info('This app is ML')
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
