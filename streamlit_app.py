e492d99a64e0a0b3f2c83f6b6fbc921a118fb639
adhikaribibek231 <159928438+adhikaribibek231@users.noreply.github.com>
2025-02-24 18:27:36 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,6 @@ with st.expander('Data visualization'):
 #data preparations
 with st.sidebar:
   st.header('Input features')
+  island=st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+  gender=st.selectbox('Gender',('male','female'))
+  bill_length_mm= st.slider('Bill length (mm)', 32.1, 59.6,43.9)
